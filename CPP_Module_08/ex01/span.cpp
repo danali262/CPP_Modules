006a4b5464e769de03bbc766463a10ee5d923162
Danai <dgiannop@codam.student.nl>
2021-12-17 16:14:47 +0100

    p r o g r e s s . . .

--- CPP_Module_08/ex01/span.cpp
+++ CPP_Module_08/ex01/span.cpp
@@ -1,6 +1,29 @@
 #include "span.hpp"
 
-Span::Span(unsigned int N)
+Span::Span(unsigned int n)
 {
-    
+    this->_size = n;
+	this->_pos = 0;
+}
+
+Span::Span(const Span &s)
+{
+    *this = s;
+}
+
+Span    &Span::operator=(const Span &s)
+{
+    if(this != &s)
+	{
+        this->_size = s._size;
+		this->_pos = 0;
+	}
+    return (*this);
+}
+
+void	Span::addNumber(int nbr)
+{
+	if (this->_pos > this->_size)
+		throw NoSpaceException();
+	this->_vect.push_back(nbr);	
 }
--- CPP_Module_08/ex01/span.hpp
+++ CPP_Module_08/ex01/span.hpp
@@ -3,14 +3,30 @@
 
 #include <algorithm>
 #include <vector>
+#include <exception>
 
 class Span
 {
 	private:
 		std::vector<int>	_vect;
 		unsigned int		_size;
+		unsigned int		_pos;
 	public:
-    	Span(unsigned int N);
+    	Span(unsigned int n);
+		Span(const Span &s);
+		Span	&operator=(const Span &s);
+
+		void	addNumber(int nbr);
+
+		class NoSpaceException : public std::exception
+		{
+			public:
+				const char *what() const throw()
+				{
+					return ("Not enough space in the vector to add number.\n");
+				}
+		};
+
     	~Span(void);
 };
 
